151e7f9bb5ddcd1054b2a591388b8c9450457ab9
luqqy <you@example.com>
2020-10-12 08:18:38 +0300

    updated the models files

--- gallery/media/models.py
+++ gallery/media/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
+
 
 class Editor(models.Model):
-    title = models.CharField(max_length =60)
-    post = models.TextField()
-    editor = models.ForeignKey(Editor)
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
 
 
     def __str__(self):
         return self.name
+
+   class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)         
