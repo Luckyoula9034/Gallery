bae563558e4f63a15696a8fd67f5e5681e58c8f4
luqqy <you@example.com>
2020-10-12 08:37:29 +0300

    updated models .py

--- gallery/media/tests.py
+++ gallery/media/tests.py
@@ -7,5 +7,10 @@ class EditorTestClass(TestCase):
 
     # Set up method
     def setUp(self):
-        self.james= Editor(first_name = 'lucky', last_name ='Oula', email ='luckyoula.com')
+        self.lucky= Editor(first_name = 'lucky', last_name ='Oula', email ='luckyoula.com')
+    # Testing Save Method
+    def test_save_method(self):
+        self.lucky.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)    
 
