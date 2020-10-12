f0520ce71f3da41fde8107f4eebaefa6581e5a90
luqqy <you@example.com>
2020-10-12 04:07:37 +0300

    added a date function that will be responsible for returning picture added for aspecific date or day

--- gallery/media/views.py
+++ gallery/media/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the gallery')
+
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
