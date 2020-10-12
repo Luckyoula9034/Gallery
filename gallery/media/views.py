4c46e05a7d5075053fb6dd4e52074a9507940b36
luqqy <you@example.com>
2020-10-12 04:28:47 +0300

    imported the Http404 function from django.http module.

--- gallery/media/views.py
+++ gallery/media/views.py
@@ -1,19 +1,21 @@
+
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime as dt
+
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the gallery')
+    return HttpResponse('Welcome to the Gallery')
 
 def media_of_day(request):
     date = dt.date.today()
-
+    
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
     day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1>Media for {day} {date.day}-{date.month}-{date.year}</h1>
+                <h1> Media for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
@@ -28,4 +30,23 @@ def convert_dates(dates):
 
     # Returning the actual day of the week
     day = days[day_number]
-    return day    
+    return day
+
+def past_days_media(request,past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Media for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
