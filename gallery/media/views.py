fce30bb8eeac63b5d1230c9f4a0f91bd9607ea74
luqqy <you@example.com>
2020-10-12 04:15:28 +0300

    called convert_dates function inside our news_of_day view function and we store the day inside a variable day.

--- gallery/media/views.py
+++ gallery/media/views.py
@@ -7,10 +7,13 @@ def welcome(request):
 
 def media_of_day(request):
     date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
